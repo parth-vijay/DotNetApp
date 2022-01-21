c247113a33a3b21b1c0ec6c243bcec47f851d4c0
parth1625 <parth@segnotech.com>
2022-01-21 15:25:30 +0530

    assign values in variable file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "docker_username" {
+  description = "Docker Hub username"
+  type        = string
+  sensitive   = true
+}
+
+variable "docker_password" {
+  description = "Docker Hub password"
+  type        = string
+  sensitive   = true
+}
